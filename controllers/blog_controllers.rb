2bbee76830d6377c4c67a22094eeeb44322c1e3a
Tech-A11 <Tech-A11@Tech-A11s-MacBook-Air.local>
2017-11-16 16:31:59 +0000

    lab today 16/11/17

--- controllers/blog_controllers.rb
+++ controllers/blog_controllers.rb
@@ -11,25 +11,33 @@ class BlogsController < Sinatra::Base
   $photos = [{
 	 id: 0,
 	 title: "Photo 1",
-	 body: "This is the first post"
+	 body: "This is the first post",
+   img: "http://fillmurray.com/300/300"
 },
 {
     id: 1,
     title: "Photo 2",
-    body: "This is the second post"
+    body: "This is the second post",
+    img: "http://fillmurray.com/300/300"
 },
 {
     id: 2,
     title: "Photo 3",
-    body: "This is the third post"
+    body: "This is the third post",
+    img: "http://fillmurray.com/300/300"
 }];
+
+
 
   get "/" do
     @title = "Photos homepage"
-    erb :'photos/index'
+    erb :'photos/home'
   end
 
+
+
   get "/photos" do
+
     @title = 'photos'
     @photos = $photos
     erb :'photos/index'
@@ -47,7 +55,10 @@ class BlogsController < Sinatra::Base
   end
 
   get "/photos/:id/edit" do
-    "Edit: #{params[:id]}"
+    @title = 'Edit Page'
+    id = params[:id].to_i
+    @photo = $photos[id]
+    erb :'photos/edit'
   end
 
 end
